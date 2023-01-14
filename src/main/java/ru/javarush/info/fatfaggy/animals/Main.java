8d48deb718ad4676772734334291e1c1d45e2e97
Даниил Соловьёв <daniil.soloviev@itvert.com>
2023-01-14 16:58:49 +0300

    fix entities

--- src/main/java/ru/javarush/info/fatfaggy/animals/Main.java
+++ src/main/java/ru/javarush/info/fatfaggy/animals/Main.java
@@ -1,2 +1,20 @@
-package ru.javarush.info.fatfaggy.animals;public class Main {
+package ru.javarush.info.fatfaggy.animals;
+
+import entities.Cat;
+import entities.Dog;
+import entities.Parrot;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("entities");
+        Cat cat = applicationContext.getBean(Cat.class);
+        Dog dog = (Dog) applicationContext.getBean("dog");
+        Parrot parrot = applicationContext.getBean("parrot - Kesha", Parrot.class);
+
+        System.out.println(cat.getName());
+        System.out.println(dog.getName());
+        System.out.println(parrot.getName());
+    }
 }
