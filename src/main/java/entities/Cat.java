4865c8a5f5475b462e15ab7445544e4e9c22008c
Daniil <91124233+JojiDS@users.noreply.github.com>
2023-01-14 17:38:50 +0300

    Revert "java config"

--- src/main/java/entities/Cat.java
+++ src/main/java/entities/Cat.java
@@ -1,15 +1,2 @@
-package entities;
-
-import org.springframework.stereotype.Component;
-
-public class Cat {
-    private String name = "Барсик";
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+package entities;public class Cat {
 }
--- src/main/java/entities/Dog.java
+++ src/main/java/entities/Dog.java
@@ -1,15 +1,2 @@
-package entities;
-
-import org.springframework.stereotype.Component;
-
-public class Dog {
-    private String name = "Бобка";
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+package entities;public class Dog {
 }
